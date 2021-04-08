ae781f8227f8f9c8fb08b1e9c6a362f6a4eba61e
Ishors <antoine.cap@hotmail.fr>
2021-04-08 16:55:52 +0200

    :lipstick: 2nd UI design test for bucketlist

--- www/bucket-list/bucket-list.component.js
+++ www/bucket-list/bucket-list.component.js
@@ -7,15 +7,15 @@ angular.
     templateUrl: 'bucket-list/bucket-list.template.html',
     controller: ['$ionicPopup', function BucketListController($ionicPopup) {
       var wishList =[
-        {title: "Sauter en parachute", description: "Sauter en parachute accompagné puis apprendre à le faire solo", dueDate: 2021, photo: "sauter-en-parachute.png", completed: false},
-        {title: "Chiens de traîneaux", description: "Le must serait dans les pays du Nord, sinon les pyrénées c'est pas mal aussi", dueDate: 2022, photo: "chiens-de-traîneaux.png", completed: false},
-        {title: "Voir une aurore boréale", description: "Aucune description", dueDate: 2050, photo: "voir-une-aurore-boreale.png", completed: false},
-        {title: "Travailler en Antarctique", description: "Pour une durée de 12 à 18 mois loin de tout", dueDate: 2027, photo: "travailler-en-antarctique.png", completed: false},
-        {title: "Monter ma marque", description: "Oyats vie", dueDate: 2022, photo: "monter-ma-marque.png", completed: false},
-        {title: "Visiter l'Islande", description: "Faire le tour de l'île en van en 2 semaines", dueDate: 2025, photo: "visiter-lislande.png", completed: false},
-        {title: "Road-trip Italie du Nord", description: "En van", dueDate: 2023, photo: "road-trip-italie-du-nord.png", completed: false},
-        {title: "Voyage Amérique du Sud", description: "Salar et compagnie, me voilà !", dueDate: 2023, photo: "voyage-amerique-du-sud.png", completed: false},
-        {title: "Vivre dans les bois", description: "Laisse tomber le kiff interdimentionnel", dueDate: 2070, photo: "vivre-dans-les-bois.png", completed: false}
+        {id: 1, title: "Sauter en parachute", description: "Sauter en parachute accompagné puis apprendre à le faire solo", dueDate: 2021, photo: "sauter-en-parachute.png", completed: false},
+        {id: 2, title: "Chiens de traîneaux", description: "Le must serait dans les pays du Nord, sinon les pyrénées c'est pas mal aussi", dueDate: 2022, photo: "chiens-de-traîneaux.png", completed: false},
+        {id: 3, title: "Voir une aurore boréale", description: "Aucune description", dueDate: 2050, photo: "voir-une-aurore-boreale.png", completed: false},
+        {id: 4, title: "Travailler en Antarctique", description: "Pour une durée de 12 à 18 mois loin de tout", dueDate: 2027, photo: "travailler-en-antarctique.png", completed: false},
+        {id: 5, title: "Monter ma marque", description: "Oyats vie", dueDate: 2022, photo: "monter-ma-marque.png", completed: false},
+        {id: 6, title: "Visiter l'Islande", description: "Faire le tour de l'île en van en 2 semaines", dueDate: 2025, photo: "visiter-lislande.png", completed: false},
+        {id: 7, title: "Road-trip Italie du Nord", description: "En van", dueDate: 2023, photo: "road-trip-italie-du-nord.png", completed: false},
+        {id: 8, title: "Voyage Amérique du Sud", description: "Salar et compagnie, me voilà !", dueDate: 2023, photo: "voyage-amerique-du-sud.png", completed: false},
+        {id: 9, title: "Vivre dans les bois", description: "Laisse tomber le kiff interdimentionnel", dueDate: 2070, photo: "vivre-dans-les-bois.png", completed: false}
       ];
       wishList.sort(function(a, b) {
         return a.dueDate - b.dueDate;
